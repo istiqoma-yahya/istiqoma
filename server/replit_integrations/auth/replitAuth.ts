bd338e6d611e8af9d102743c52fe8fb446156f8f
yahyaekananta1 <51715894-yahyaekananta1@users.noreply.replit.com>
2026-02-19 05:07:33 +0000

    Restored to 'e9df8c5fdc9f27472c9a707348c5a0327f9f5f13'
    Replit-Restored-To: e9df8c5fdc9f27472c9a707348c5a0327f9f5f13

--- server/replit_integrations/auth/replitAuth.ts
+++ server/replit_integrations/auth/replitAuth.ts
@@ -184,16 +184,13 @@ export async function setupAuth(app: Express) {
   });
 
   app.get("/api/logout", (req, res) => {
-    const redirectUrl = client.buildEndSessionUrl(config, {
-      client_id: process.env.REPL_ID!,
-      post_logout_redirect_uri: `${req.protocol}://${req.hostname}`,
-    }).href;
-
     req.logout(() => {
-      req.session.destroy(() => {
-        res.clearCookie("connect.sid");
-        res.redirect(redirectUrl);
-      });
+      res.redirect(
+        client.buildEndSessionUrl(config, {
+          client_id: process.env.REPL_ID!,
+          post_logout_redirect_uri: `${req.protocol}://${req.hostname}`,
+        }).href
+      );
     });
   });
 }
